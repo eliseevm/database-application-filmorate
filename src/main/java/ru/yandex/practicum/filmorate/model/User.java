d88c242a27e6dd885d20fbe9ca55414768568db7
Max Eliseev <wakuum1@yandex.ru>
2022-08-03 13:18:34 +0300

    Исправить версию плагина на 2.7.2 Внести изменения в метод Исправить версию WEB на 2.7.1 Заменить снапшот на 2.7.2.

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -12,10 +10,8 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class User {
     private Integer id;
-    @NotBlank
     private String login;
     private String name;
-    @Email
     private String email;
     private LocalDate birthday;
 }
